21a14dc37796843d6728bbf4cfe50a1238d1cce2
arunp-mericaninc <arunp@mericaninc.com>
2023-06-29 10:03:23 +0530

    completed google authentication

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -81,7 +81,7 @@ const Login = () => {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
                 <span
-                  onClick={() => router.push("signUp")}
+                  onClick={() => router.push("/signUp")}
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Sign up
